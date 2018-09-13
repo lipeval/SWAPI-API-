9940df9a30ef7fd021c943f09665eb7d982764e0
Felipe Vallina <felipeval@Felipes-MacBook-Pro.local>
2018-09-13 15:57:34 -0500

    showing info

--- routes/index.js
+++ routes/index.js
@@ -10,40 +10,40 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-
+router.get('/people', (req, res)=>{
+  fetch('https://swapi.co/api/people/')
+  .then(results => results.json()) 
+    .then(info => {
+    // console.log(info)
+    res.render('people', {info});
+  });
+});
 
 
 router.get('/films',  (req, res) => {
   fetch('https://swapi.co/api/films/')
   .then(results => results.json())
   .then(info => {
-    // console.log(info)
-    res.render('info', info); 
+    res.render('info', {info}); 
   });
 });
 
-router.get('/people', (req, res)=>{
-  fetch('https://swapi.co/api/people/')
-  .then(results => results.json()) 
-    .then(info => {
-    // console.log(info)
-    res.render('people', info);
-  });
-});
+
 
 router.get('/planets', (req, res)=>{
   fetch('https://swapi.co/api/planets/')
   .then(results => results.json())
   .then(info =>{
-    res.render('planets', info)
+    res.render('planets', {info})
   })
 })  
 
-router.get('species', (req, res)=>{
+router.get('/species', (req, res)=>{
   fetch('https://swapi.co/api/species/')
   .then(results => results.json())
   .then(info =>{
-    res.render('species', info)
+    console.log(info)
+    res.render('species', {info})
   })
 })
 
